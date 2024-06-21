55f03309a2dfaa958ba24b807c88c2710d53d150
CristianoMoura86 <168308456+CristianoMoura86@users.noreply.github.com>
2024-06-21 14:26:17 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log("digite o nom do seu jogador")
+
+let nickname="nosferato"
+
+console.log("bem vindo "+nickname)
+
+const Ferro = 100;
+const Bronze = 1001;
+const Prata = 2001;
+const Ouro = 5001;
+const Platina = 7001;
+const Ascendente = 8001;
+
+ console.log("voce precisa voutar ao jogo")
+
+ console.log("a princesa foi capiturada ao castelo de dracula")
+
+ console.log("rapido seu cavalo esta esperando")
+
+
+
+
+
